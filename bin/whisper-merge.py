f720a817093c869826d3c7983f8319ecb32089c0
PARITOSH <paritosh.walvekar@github.com>
2015-08-26 12:30:38 +0530

    Complete v1.0.1

---
+++ bin/whisper-merge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import signal
+import optparse
+
+try:
+  import whisper
+except ImportError:
+  raise SystemExit('[ERROR] Please make sure whisper is installed properly')
+
+# Ignore SIGPIPE
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+option_parser = optparse.OptionParser(
+    usage='''%prog [options] from_path to_path''')
+
+(options, args) = option_parser.parse_args()
+
+if len(args) < 2:
+  option_parser.print_help()
+  sys.exit(1)
+
+path_from = args[0]
+path_to = args[1]
+
+for filename in (path_from, path_to):
+   if not os.path.exists(filename):
+       raise SystemExit('[ERROR] File "%s" does not exist!' % filename)
+
+whisper.merge(path_from, path_to)
---
+++ index.js
@@ -0,0 +1,39 @@
+var ps = require('child_process');
+var baseCommand = "sudo python bin/whisper-";
+
+module.exports.create = function(absDir, name, pointPattern, callback){
+	var command = baseCommand + "create.py " + absDir + name + ".wsp " + pointPattern;
+	var cmd = ps.exec(command);
+	cmd.on('close', function(){
+		callback();
+	});
+}
+
+module.exports.update = function(absPath, timeSeriesArray, callback){
+	var command = '';
+	keys = Object.keys(timeSeriesArray);
+	for (var i = 0; i < keys.length; i++) {
+		command = command + keys[i] + ' ';
+	};
+	command = baseCommand + "update.py " + absPath + command;
+	var cmd = ps.exec(command);
+	cmd.on('close', function(data){
+		callback();
+	});
+}
+
+module.exports.fill = function(srcPath, dstPath, callback){
+	var command = baseCommand + "fill.py " + srcPath + " " + dstPath;
+	var cmd = ps.exec(command);
+	cmd.on('close', function(){
+		callback();
+	});
+}
+
+module.exports.merge = function(fromPath, toPath, callback){
+	var command =  baseCommand + "merge.py " + fromPath + " " + toPath;
+	var cmd  = ps.exec(command);
+	cmd.on('close', function(){
+		callback();
+	});
+}
